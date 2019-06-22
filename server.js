ae3604708089f7790b109656a93c002af41ca3eb
JamesT <tagorda@gmail.com>
2019-06-22 11:26:38 -0400

    Fix heroku deploy mongodb

--- server.js
+++ server.js
@@ -37,8 +37,10 @@ app.use(router);
 
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/demodata", { useNewUrlParser: true });
+//mongoose.connect("mongodb://localhost/demodata", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/demodata";
 
+mongoose.connect(MONGODB_URI);
 // Start the server
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
